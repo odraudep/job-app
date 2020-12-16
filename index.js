b6394aae16553270b2dfc3e5404fe66a111d2c58
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2020-12-16 20:23:00 -0300

    base done

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const session = require("express-session");
 
+// Config
+app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("Test");
+  res.render("home");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Page not found!");
 });
 
 // Others
 const PORT = 8081;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log("Server running");
+});
