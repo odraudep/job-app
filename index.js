40e72e291c35e8bacc0976de536d9828c72a4b4c
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2020-12-17 13:25:33 -0300

    branch end

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
-const handlebars = require("express-handlebars");
-const path = require("path");
 const session = require("express-session");
 const flash = require("connect-flash");
+const path = require("path");
+const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const router = require("./routes/router");
+const users = require("./routes/users");
 
 //-----Config
 // Middleware
 app.use(session({
   secret: "jobapp",
   resave: true,
-  saveUninitialized: true,
-  cookie: {secure: true}
+  saveUninitialized: true
 }));
+
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -25,13 +28,28 @@ app.use((req, res, next) => {
 // Path
 app.use(express.static(path.join(__dirname, "public")));
 
+// Engine
 app.engine("handlebars", handlebars({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-//-----Routes
-app.get("/", (req, res) => {
-  res.render("home");
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// DataBase connection
+mongoose.connect("mongodb://localhost/jobapp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+}).then(() => {
+  console.log("Connected to the MongoDB");
+}).catch((err) => {
+  console.log("That's an error to connect to the database");
 });
+
+//-----Routes
+app.use(router)
+app.use("/users", users);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(__dirname + "/html/404.html");
